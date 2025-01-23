937bd54b4e20fa9857ad4d4d4977a4e3dd6bb398
mdmostafa15 <mostafaka321@gmail.com>
2025-01-23 19:35:26 +0600

    ToDo modify with fake api

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 let data = [];
-
-if (str = localStorage.getItem("data")) {
-    data = JSON.parse(str);
+async function getData(todos, obj) {
+    const x = await fetch(`https://jsonplaceholder.typicode.com/${todos}`, obj);
+    const y = await x.json();
+    console.log(y);
+    
+    runTodo(y); 
 }
 
 // access ul element
@@ -11,7 +14,6 @@ const addItemBtn = document.getElementsByClassName("btn")[0];
 const modal = document.getElementsByClassName("modal")[0];
 const btn = document.getElementsByClassName("group-btn")[0].children;
 const title = document.getElementsByName("task-title")[0];
-const description = document.getElementsByName("task-description")[0];
 
 const snackbar = document.getElementsByClassName("snackbar")[0];
 
@@ -26,6 +28,7 @@ function shwToast(msg) {
 // add items to list using data
 function readItems() {
     let temp;
+    
     if (data.length > 0) {
             temp = data.map((item, index)=>{
             // todo..
@@ -34,25 +37,25 @@ function readItems() {
             const div1 = document.createElement("div");
             const div2 = document.createElement("div");
             const listTitle =  document.createElement("p");
-            const listDescription = document.createElement("p");
+            // const listDescription = document.createElement("p");
             const iconEdit = document.createElement("i");
             const iconDelete = document.createElement("i");
                 
             // event listener
-            iconEdit.addEventListener("click", ()=>{updateItem(item)});
+            iconEdit.addEventListener("click", ()=>{updateItem(item, index)});
             iconDelete.addEventListener("click", ()=>{deleteItem(item)});
             div1.addEventListener("dblclick", ()=>{isComplete(item, index)});
     
             //adding text contents to p and i elements
             listTitle.textContent = item.title;
-            listDescription.textContent = item.description;
+            // listDescription.textContent = item.description;
             iconEdit.textContent = "border_color";
             iconDelete.textContent = "delete";
     
             // Appanding contents to div and li elements
             // p tags adding to div1
             div1.appendChild(listTitle).setAttribute("class","list-title");
-            div1.appendChild(listDescription);
+            // div1.appendChild(listDescription);
             // i tags adding to div2
             div2.appendChild(iconEdit).setAttribute("class", "material-icons btn-edit");
             div2.appendChild(iconDelete).setAttribute("class", "material-icons btn-delete");
@@ -60,7 +63,7 @@ function readItems() {
             li.appendChild(div1);
             li.appendChild(div2);
 
-            if (item.flag) {
+            if (item.completed) {
                 li.classList.add("complete"); 
                 iconEdit.style.visibility="hidden";
             }
@@ -87,85 +90,93 @@ function readItems() {
 
 // flag set to data 
 function isComplete(itm, i) {
-    const temp = itm.flag?false:true;
+    const temp = itm.completed?false:true;
     data[i]= {
         ...itm,
-        flag: temp
+        completed: temp
     }; 
-
-    localStorage.setItem("data",JSON.stringify(data));
-    data = JSON.parse(localStorage.getItem("data"));
-   
-    ul.replaceChildren(...readItems());
+    runTodo(data);
     //done
 }
 
 // adding item to list
-function createItem() {
-    if(title.value != "" && description.value != ""){
+async function createItem() {
+    if(title.value != ""){
         // add task to data 
-        data.push({
-            id: (data.length>0)?data[data.length - 1].id + 1: 101,
-            title: title.value,
-            description: description.value,
-            flag: false
-        })
+        try {
+        const x = await fetch(`https://jsonplaceholder.typicode.com/todos`, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    title: title.value,
+                    completed: false,
+                })
+            })
+            data.push(await x.json());
+            hideModal();
+            runTodo(data);
+        } catch (error) {
+            console.log(error);
+        }        
     }
-    
-    localStorage.setItem("data",JSON.stringify(data));
-    data = JSON.parse(localStorage.getItem("data"));
-    
-    hideModal();
-    ul.replaceChildren(...readItems());
     shwToast("List Item is Inserted!!!");
     //done
 }
 
 // updating item of list
-let gId=0;
-function updateItem(item) {
-    gId= item.id;
+let gId=0, gIdx=0;
+function updateItem(item, i) {
+    gId = item.id;
+    gIdx= i;
     title.value = item.title;
-    description.value = item.description;
-
     // update task  :action button
     btn[0].value= "update";
     btn[0].addEventListener("click", update);
     showModal();
+    btn[0].removeEventListener("click", createItem);
     //done
 }
 
 // update item
-function update() {
-    const idx = data.findIndex((itm)=>{
-        return itm.id === gId;
-    })
-    data[idx]= {
-        id:gId,
-        title: title.value,
-        description: description.value,
-        flag: false
-    };  
+async function update() {
+    try {
+        const x = await fetch(`https://jsonplaceholder.typicode.com/todos/${gId}`, {
+            method: 'PATCH',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                
+                title: title.value,
+                completed: false,
+            })
+        });
+        // update in local
+        data[gIdx]= await x.json();
+        hideModal();
+        runTodo(data);  
+    } catch (error) {
+        console.log(error);
+    }              
 
-    localStorage.setItem("data",JSON.stringify(data));
-    data = JSON.parse(localStorage.getItem("data"));
-    
-    hideModal();
-    ul.replaceChildren(...readItems());
     shwToast("List Item is Updated!!!");
     //done
 }
 
 // delete item from list
-function deleteItem(item) {
-    const temp = data.filter((itm)=>{
-        return itm.id != item.id;
-    });  
+async function deleteItem(item) {
+    try {
+        const x = await fetch(`https://jsonplaceholder.typicode.com/todos/${item.id}`, {
+            method: 'DELETE'
+        });
 
-    localStorage.setItem("data",JSON.stringify(temp));
-    data = JSON.parse(localStorage.getItem("data"));
+        hideModal();
+        const temp = data.filter((itm)=>{
+            return itm.id != item.id;
+        });  
+        runTodo(temp);
+    } catch (error) {
+        console.log(error);
+    }  
     
-    ul.replaceChildren(...readItems());
     shwToast("List Item is Deleted!!!");
     //done
 }
@@ -193,7 +204,6 @@ function hideModal() {
     btn[1].removeEventListener("click", hideModal);
 
     title.value="";
-    description.value="";
     btn[0].value = "add";
     // hidding modal
     modal.parentElement.classList.add("hidden");
@@ -201,12 +211,18 @@ function hideModal() {
     addItemBtn.addEventListener("click", showModal);  
 }
 
-function runTodo() {   
+function runTodo(apiData) {   
     // show modal with click
     addItemBtn.addEventListener("click", showModal);
+    data = apiData;
     // appending or replace li to ul
     ul.replaceChildren(...readItems());
 }
 
 
-runTodo();
+try {
+    getData("todos");
+} catch (error) {
+    console.log(error);
+    
+}
